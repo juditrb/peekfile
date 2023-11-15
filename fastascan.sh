fdb4ad3c6a7a1c83dcf16fdb2629ec34209d3632
juditrb <jude.rb@gmail.com>
2023-11-15 17:44:16 +0100

    sixth commit

--- fastascan.sh
+++ fastascan.sh
@@ -2,11 +2,14 @@
 
 ##You can search for fasta/fa files in the folder you want, but if no path is written, use the current folder by default(.)
 
+#First argument
+
 if [[ -n "$1" ]]; then
 	folder="$1"
 else
 	folder="."
 fi
+
 
 
 fa_fasta_files=$(find "$folder" -type f -name "*.fa" -o -name "*.fasta")
@@ -17,8 +20,17 @@ echo "Fasta Files: "
 
 
 files_count=0
+total_id_count=0
+
 for file in $fa_fasta_files; do
-    ((files_count++)); awk '/^>/{gsub(/>/, "",$1); print $1; exit}' "$file" | uniq -c
+    ((files_count++)); IDs=$(awk '/^>/{gsub(/>/, "",$1); print $1; exit}' "$file" | uniq -c)
+	echo "$IDs"
+	Id_count=0
+	for Id in $IDs; do
+		((Id_count++))		
+	done
+	((total_id_count += Id_count))	
 done
 
 echo "Total number of files: $files_count"
+echo "Total number of IDs: $total_id_count"
