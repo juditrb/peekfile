6971112a0fcaa055c4fe6bc111bb02276d22bfc7
juditrb <jude.rb@gmail.com>
2023-11-16 19:05:13 +0100

    ninth commit

--- fastascan.sh
+++ fastascan.sh
@@ -8,6 +8,7 @@ if [[ -n "$1" ]]; then
 else
 	folder="."
 fi
+
 #Second argument: a number of lines N (by default: 0)
 
 if [[ -n "$2" ]]; then
@@ -16,29 +17,43 @@ else
 	num_lines="0"
 fi
 
-#Searching the fa/fasta files
+#####REPORT#####
+
+##Finding the fa/fasta files
 
 fa_fasta_files=$(find "$folder" -type f -name "*.fa" -o -name "*.fasta")
 
 echo "Folder: $folder"
 
-echo "Fa/fasta IDs: "
+echo "Fa/fasta IDs of each file: "
 
+
+##Initializing counter variables
 
 files_count=0
 total_id_count=0
 
+## "For" loop to count the files, to print the files IDs and to count the total IDs
+
 for file in $fa_fasta_files; do
+
 	if [[ $(wc -l < "$file") -ge $num_lines ]]; then
 
-    	((files_count++)); IDs=$(awk '/^>/{gsub(/>/, "",$1); print $1; exit}' "$file" | uniq -c)
+    	((files_count++)); echo "$file"; IDs=$(awk '/^>/{gsub(/>/, "",$1); print 	$1; exit}' "$file" | uniq -c)
 	echo "$IDs"
+
 	Id_count=0
 	for Id in $IDs; do
 		((Id_count++))		
 	done
 	((total_id_count += Id_count))
 
+	fi
+	
+	if [[ -h "$file" ]]; then
+		echo "SYMLINK"
+	else
+		echo "NO SYMLINK"
 	fi
 done
 
